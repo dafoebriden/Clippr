5be019f1a2bfa4ff24f4aae716e0dc8b1a041a72
MeganHancock <mgnhnck@gmail.com>
2024-02-23 12:23:36 -0700

    ClipSchema

---
+++ server/src/models/Clip.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+
+export const ClipSchema = new Schema(
+    {
+        authorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+        caption: { type: String, required: true, maxLength: 85, minLength: 2 },
+        gifUrl: { type: URL, required: true, maxLength: 500 }
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true }
+    }
+)
