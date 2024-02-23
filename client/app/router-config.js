a4bca7fa89c9dc1841be21b2053a4027f80c2d52
Briden Dafoe <dafoebriden@gmail.com>
2024-02-23 12:46:36 -0700

    header

--- client/app/router-config.js
+++ client/app/router-config.js
@@ -8,18 +8,7 @@ export const router = new Router([
   {
     path: '',
     controllers: [HomeController],
-    view: /*html*/`
-    <div class="bg-white p-3">
-      <div class="card-body">
-        <p>Home Page</p>
-        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
-      </div>
-    </div>
-    `
-  },
-  {
-    path: '#/about',
-    view: 'app/views/AboutView.html'
+    view: 'app/views/HomeView.html'
   },
   {
     path: '#/account',
